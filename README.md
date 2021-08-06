66501c54fd94e30038c2fc00dc0c4ca0e1ef1371
Jose Ustra <josecarlos@ustrajunior.com>
2021-08-06 22:21:37 +0100

    Improves the visual selection and map keys

--- README.md
+++ README.md
@@ -9,13 +9,21 @@ Converts Elixir maps from atom to string and vice versa.
 
 ```lua
 use 'ustrajunior/ex_maps'
+
+lua require("ex_maps").setup {
+	create_mappings = true,
+	line_mapping = "mtt",
+	operator_mapping = "mt",
+}
 ```
 
 ## How to use
-```lua
-lua require("ex_maps").atom_to_string()
-lua require("ex_maps").string_to_atom()
-```
+
+You have basically two options. Put the cursor in the line you want to convert and press "mtt", or in visual mode, press "mt".
 
 ## demo
 ![screenshot](./screenshots/ex_maps.gif)
+
+## Credits
+
+I based and copied some code from [nvim-comment](https://github.com/terrortylor/nvim-comment) for the visual selection, so thanks [Alex Tylor](https://github.com/terrortylor).
--- lua/ex_maps/init.lua
+++ lua/ex_maps/init.lua
@@ -1,44 +1,84 @@
-local function visual_selection_range()
-	local _, csrow, cscol, _ = unpack(vim.fn.getpos("'<"))
-	local _, cerow, cecol, _ = unpack(vim.fn.getpos("'>"))
-	if csrow < cerow or (csrow == cerow and cscol <= cecol) then
-		return csrow, cscol - 1, cerow, cecol
-	else
-		return cerow, cecol - 1, csrow, cscol
-	end
-end
+local api = vim.api
 
-local function replace(pattern, replacement)
-	local startline, _, endline, _ = visual_selection_range()
+local M = {}
 
-	local lines = vim.api.nvim_buf_get_lines(0, startline-1, endline, false)
-
-	for i, line in ipairs(lines) do
-		local new_line = string.gsub(line, pattern, replacement)
-
-		local current_line = startline-2+i
-		local next_line = current_line+1
-
-		vim.api.nvim_buf_set_lines(0, current_line, next_line, false, {new_line})
-	end
-end
-
-local function atom_to_string(_)
-	local pattern = '(%s+)(.+):'
-	local replacement =	"%1\"%2\" =>"
-
-	replace(pattern, replacement)
-end
-
-local function string_to_atom(_)
-	local pattern = '\"(.+)\"(%s=>)'
-	local replacement = "%1:"
-
-	replace(pattern, replacement)
-end
-
-return {
-	atom_to_string = atom_to_string,
-	string_to_atom = string_to_atom
+M.config = {
+	create_mappings = true,
+	line_mapping = "mtt",
+	operator_mapping = "mt",
 }
 
+local atom_pattern = '(%S*):'
+local string_pattern = '\"(.+)\"(%s=>)'
+
+function M.operator(mode)
+	local line1, line2
+
+  if not mode then
+    line1 = api.nvim_win_get_cursor(0)[1]
+    line2 = line1
+  elseif mode:match("[vV]") then
+    line1 = api.nvim_buf_get_mark(0, "<")[1]
+    line2 = api.nvim_buf_get_mark(0, ">")[1]
+  else
+    line1 = api.nvim_buf_get_mark(0, "[")[1]
+    line2 = api.nvim_buf_get_mark(0, "]")[1]
+  end
+
+	M.toggle(line1, line2)
+end
+
+local function atom_to_string(line)
+	local replacement =	"\"%1\" =>"
+
+	return string.gsub(line, atom_pattern, replacement)
+end
+
+local function string_to_atom(line)
+	local replacement = "%1:"
+
+	return string.gsub(line, string_pattern, replacement)
+end
+
+local function replace(startline, endline)
+	local lines = api.nvim_buf_get_lines(0, startline-1, endline, false)
+	if not lines then return end
+
+	for i, line in ipairs(lines) do
+		if line:match(atom_pattern) then
+			lines[i] = atom_to_string(line)
+		else
+			lines[i] = string_to_atom(line)
+		end
+	end
+
+	api.nvim_buf_set_lines(0, startline-1, endline, false, lines)
+end
+
+function M.toggle(line1, line2)
+	replace(line1, line2)
+end
+
+function M.setup(user_opts)
+  M.config = vim.tbl_extend('force', M.config, user_opts or {})
+
+  local vim_func = [[
+  function! MapOperator(type) abort
+    let reg_save = @@
+    execute "lua require('ex_maps').operator('" . a:type. "')"
+    let @@ = reg_save
+  endfunction
+  ]]
+  vim.api.nvim_call_function("execute", {vim_func})
+  vim.api.nvim_command("command! -range MapToggle lua require('ex_maps').toggle(<line1>, <line2>)")
+
+  if M.config.create_mappings then
+    local opts = {noremap = true, silent = true}
+
+    api.nvim_set_keymap("n", M.config.line_mapping, "<Cmd>set operatorfunc=MapOperator<CR>g@l", opts)
+    api.nvim_set_keymap("n", M.config.operator_mapping, "<Cmd>set operatorfunc=MapOperator<CR>g@", opts)
+    api.nvim_set_keymap("v", M.config.operator_mapping, ":<C-u>call MapOperator(visualmode())<CR>", opts)
+  end
+end
+
+return M
